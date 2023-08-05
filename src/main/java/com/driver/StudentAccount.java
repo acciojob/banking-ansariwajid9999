20036ca714014e3af8783dc7c3fb21254301ab94
Wajid <ansariwajid9999@gmail.com>
2023-08-05 14:24:07 +0530

    previous commit

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -4,20 +4,16 @@ public class StudentAccount extends BankAccount{
 
     String  institutionName;
 
-    public StudentAccount(String name, double balance, String  institutionName)
-    {
-        //minimum balance is 0 by default
+    public StudentAccount(String name, double balance, String  institutionName) {
         super(name,balance,0);
         this.institutionName=institutionName;
 
     }
-
-    public String getInstitutionName()
-    {
-        return institutionName;
+    public String getInstitutionName(){
+        return this.institutionName;
+    }
+    public void setInstitutionName(String institutionName){
+        this.institutionName=institutionName;
     }
 
-    public void setInstitutionName(String institutionName) {
-        this.institutionName = institutionName;
-    }
 }
